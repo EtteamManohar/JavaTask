bbe174759704b3d8e2e8d9108d0e70548d73fff7
Manohar Etteam <manoharetteam@gmail.com>
2022-08-08 23:07:25 +0530

    Task4

--- src/Task4.java
+++ src/Task4.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Task4 {
+import java.util.Scanner;
+public class Task4
+{
+    public static void main (String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        while (n != 1)
+        {
+            System.out.print(n + " ");
+            if ((n & 1) == 1)
+                n = 3 * n + 1;
+            else
+                n = n / 2;
+        }
+        System.out.print(n);
+    }
 }
+
