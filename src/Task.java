a52a9b4c8c40d515acd176154f6fe3bef9b1f557
Manohar Etteam <manoharetteam@gmail.com>
2022-08-08 15:16:52 +0530

    task

---
+++ src/Task.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Task {
+    public static void main(String[] args) {
+        int L1, L2, L3, N, X, Y, Z;
+        Scanner sc = new Scanner(System.in);
+        L1 = sc.nextInt();
+        L2 = sc.nextInt();
+        L3 = sc.nextInt();
+        N = sc.nextInt();
+        X = L1 - N;//5
+        Y = L2 - N;//15
+        Z = L3 - N;//-5
+       if(X>=0&&Y>=0&&Z>=0)
+        {
+            if (X <=Y && X <=Z) {
+                System.out.println("L1");
+            } else if (Y <= X && Y <= Z) {
+                System.out.println("L2");
+            } else {
+                System.out.println("L3");
+            }
+        }
+    }
+}
