69cbf7a9a6b1873d3cc071bfe0f6ccf261af70e6
Manohar Etteam <manoharetteam@gmail.com>
2022-08-08 17:47:37 +0530

    Task1 added

--- src/Task.java
+++ src/Task.java
@@ -11,15 +11,36 @@ public class Task {
         X = L1 - N;//5
         Y = L2 - N;//15
         Z = L3 - N;//-5
-       if(X>=0&&Y>=0&&Z>=0)
-        {
-            if (X <=Y && X <=Z) {
-                System.out.println("L1");
-            } else if (Y <= X && Y <= Z) {
+        if(X<0 && Y>=0 && Z>=0 ){
+            if(Y<Z){
                 System.out.println("L2");
-            } else {
+            }else{
                 System.out.println("L3");
             }
         }
+        if(X>=0 && Y<0 && Z>=0 ){
+            if(X<Z){
+                System.out.println("L1");
+            }else{
+                System.out.println("L3");
+            }
+        }
+        if(X>=0 && Y>=0 && Z<0 ){
+            if(X<Y){
+                System.out.println("L1");
+            }else{
+                System.out.println("L2");
+            }
+        }
+        if(X<=Y && X<=Z && X>=0 && Y>=0 && Z>=0){
+            System.out.println("L1");
+        }
+        else if(Y<=X && Y<=Z && X>=0 && Y>=0 && Z>=0){
+            System.out.println("L2");
+        }
+        else if(Z<=X && Z<=Y && X>=0 && Y>=0 && Z>=0){
+            System.out.println("L3");
+        }
+
     }
 }
